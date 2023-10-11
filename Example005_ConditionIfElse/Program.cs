2a0b45741d58ccc6f7ec1339158b22b69032b1ea
AndreyYukin <capelanya@yandex.ru>
2023-10-11 14:49:28 +0300

    новый gitignore

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя польщзователя: ");
+string usermane = Console.ReadLine();
+
+if(usermane.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(usermane);
+}
